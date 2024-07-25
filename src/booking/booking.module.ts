a8fb5a7ac516091d18846cbbfe611920f7439bb1
Umang Dabhi <ud30104@gmail.com>
2024-07-25 22:18:00 +0530

    "Error Resolved"

--- src/booking/booking.module.ts
+++ src/booking/booking.module.ts
@@ -3,9 +3,10 @@ import { BookingService } from './booking.service';
 import { BookingController } from './booking.controller';
 import { Booking } from './entities/booking.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Traveller } from 'src/traveller/entities/traveller.entity';
 
 @Module({
-  imports:[TypeOrmModule.forFeature([Booking])],
+  imports:[TypeOrmModule.forFeature([Booking,Traveller])],
   controllers: [BookingController],
   providers: [BookingService],
 })
--- src/booking/booking.service.ts
+++ src/booking/booking.service.ts
@@ -14,10 +14,25 @@ export class BookingService {
     @InjectRepository(Traveller)
     private readonly travellerRepository: Repository<Traveller>,
   ) { }
-  create(createBookingDto: CreateBookingDto) {
+  async create(createBookingDto: CreateBookingDto) {
     const { traveller, ...bookingData } = createBookingDto;
     console.log(traveller);
     console.log(bookingData);
+    if (traveller) {
+      const newTraveller = new Traveller();
+      newTraveller.name = `${traveller.firstname} ${traveller.lastname}`;
+      newTraveller.phone_no = parseInt(traveller.phone_no);
+      newTraveller.secondary_phone_no = parseInt(traveller.secondary_phone_no);
+      newTraveller.email = traveller.email;
+
+      await this.travellerRepository.save(newTraveller);
+    }
+    const newBooking = new Booking();
+    newBooking.departure_from = bookingData.departure_from;
+    newBooking.selling_price = bookingData.selling_price;
+    newBooking.advance_received = bookingData.advance_received;
+    newBooking.total_pax = bookingData.total_pax;
+    // newBooking.user = 
     return 'This action adds a new booking';
   }
 
