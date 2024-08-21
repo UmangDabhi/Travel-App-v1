1d1c18ff04fa9fe7c60496dd6ca0d6aeb3ba8f51
Umang Dabhi <ud30104@gmail.com>
2024-08-21 23:44:43 +0530

    new build added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, OnModuleInit,Injectable } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TripModule } from './trip/trip.module';
@@ -8,7 +8,24 @@ import { AuthModule } from './auth/auth.module';
 import { BookingModule } from './booking/booking.module';
 import { TravellerModule } from './traveller/traveller.module';
 import { ConfigModule } from '@nestjs/config';
+import { Booking } from './booking/entities/booking.entity';
+import { Traveller } from './traveller/entities/traveller.entity';
+import { Trip } from './trip/entities/trip.entity';
+import { User } from './user/entities/user.entity';
 
+// Create a separate service for configuration logging
+@Injectable()
+class ConfigLoggerService implements OnModuleInit {
+  onModuleInit() {
+    console.log({
+      POSTGRES_HOST: process.env.POSTGRES_HOST,
+      POSTGRES_PORT: parseInt(process.env.POSTGRES_PORT, 10),
+      POSTGRES_USER: process.env.POSTGRES_USER,
+      POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
+      POSTGRES_DB: process.env.POSTGRES_DB,
+    });
+  }
+}
 
 @Module({
   imports: [
@@ -33,6 +50,6 @@ import { ConfigModule } from '@nestjs/config';
     TravellerModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ConfigLoggerService],
 })
 export class AppModule { }
